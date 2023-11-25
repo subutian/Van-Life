ef15f554a57ce65d6b5d698b14ebe244b8adcab7
Brian <subutian@gmail.com>
2023-11-26 01:20:06 +1100

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       <h1>You got the travel plans, we got the travel vans.</h1>
       <p>
         Add adventure to your life by joining the #vanlife movement. Rent the
-        perfect van to make your perfect road trip.
+        perfect van to make your perfect road trip..
       </p>
       <Link to="vans">Find your van</Link>
     </div>
